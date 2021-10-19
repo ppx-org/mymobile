a9d5e2211769cae8ba5ddc21bfe002f1d27ac0e6
mark <6882855@qq.com>
2021-10-19 22:21:46 +0800

    t

--- src/main.js
+++ src/main.js
@@ -6,18 +6,12 @@ import App from './App.vue'
 
 
 // // 引入组件
-// import Button from 'vant/es/button';
-import {Button } from 'vant';
-// 引入组件对应的样式，若组件没有样式文件，则无须引入
-//  import 'vant/es/button/style';
+import {Button} from 'vant';
+import {List, Cell} from 'vant';
 
 const app = createApp(App)
-
-// import Vant from 'vant';
-// import 'vant/lib/index.css';
-// app.use(Vant);
-
-
 app.use(Button);
+app.use(List);
+app.use(Cell);
 
 app.mount('#app')
