c099f8e0f580fd6717f75144efa6f47f6541652e
mark <6882855@qq.com>
2021-10-20 21:40:57 +0800

    t

---
+++ src/components/App_bak2.vue
@@ -0,0 +1,64 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue";
+
+const list = ref([1,3,5]);
+const loading = ref(false);
+const finished = ref(false);
+
+const onLoad = () => {
+  // 异步更新数据
+  // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+  setTimeout(() => {
+    for (let i = 0; i < 10; i++) {
+      list.value.push(list.value.length + 1);
+    }
+
+// 加载状态结束
+    loading.value = false;
+
+// 数据全部加载完成
+    if (list.value.length >= 40) {
+      finished.value = true;
+    }
+  }, 1000);
+};
+
+// return {
+//   list,
+//   onLoad,
+//   loading,
+//   finished,
+// };
+
+</script>
+
+<template>
+
+  <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+  >
+    <van-cell v-for="item in list" :key="item" :title="item"/>
+  </van-list>
+</template>
+
+<script>
+
+import {ref} from 'vue';
+
+export default {
+  setup() {
+
+  },
+};
+
+
+</script>
+
+<style>
+</style>
